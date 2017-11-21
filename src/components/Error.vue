fa5d8b601153a57633029b8f87625c2b248f9ed1
skygodbas <2238178090@qq.com>
2017-11-21 12:45:47 +0800

    自定义错误处理组件

---
+++ src/components/Error.vue
@@ -0,0 +1,16 @@
+<template>
+<div id="div1">
+  <h1>Sorry , the page is not found.</h1>
+</div>
+</template>
+<script>
+export default {
+
+}
+</script>
+<style scoped>
+#div1{
+    background: #000;
+    color: #fff
+}
+</style>
