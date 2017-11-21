4bccf27b5cfb8118c6f98090348547bbbf4b3b5d
skygodbas <2238178090@qq.com>
2017-11-21 12:43:46 +0800

    子路由1

---
+++ src/components/Test1.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+    <h1>子路由1</h1>
+    <!--获取参数：$route.params.参数名-->
+    <h1>姓名：{{$route.params.name}} 年龄：{{$route.params.age}}</h1>
+  </div>
+</template>
+<script>
+export default {
+
+}
+</script>
+<style scoped>
+
+</style>
