53d3f788ce115af541dd4ba9f7a741c560152842
skygodbas <2238178090@qq.com>
2017-11-22 18:52:32 +0800

    Update Count.vue

--- src/components/Count.vue
+++ src/components/Count.vue
@@ -1,18 +1,43 @@
 <template>
-  <div>
-      <h1>计数器：{{$store.state.count}}</h1>
-      <button @click="$store.commit('add')">增加</button>
-      <button @click="$store.commit('reduce')">减少</button>
-  </div>
+    <div>
+        <h1>计数器: {{$store.state.count}}</h1>
+        <h1>{{count1}}</h1>
+        <button @click="$store.commit('add', 10)">加1</button>
+        <button @click="reduce">减1</button>
+        <p>
+            <button @click="addAction">+</button>
+            <button @click="reduceAction">-</button>
+            <button @click="dis">dispatch</button>
+        </p>
+    </div>
 </template>
-
 <script>
 import store from '@/vuex/store'
+import {mapState, mapMutations, mapGetters, mapActions} from 'vuex'
 export default {
-  data () {
-      return {}
+  data(){
+      return {};
   },
-  store
+  store,
+  /* computed: {
+      count(){
+        return this.$store.state.count;
+      }
+  } */
+  /* computed:mapState({
+        count1:state=>state.count
+    }) */
+    computed: {
+        ...mapState(['count']),
+        ...mapGetters(['count1'])
+    },
+    methods: {
+        ...mapMutations(['add', 'reduce']),
+        ...mapActions(['addAction', 'reduceAction']),
+        dis(){
+            this.$store.dispatch('addAction', 10)
+        }
+    }
 }
 </script>
 
