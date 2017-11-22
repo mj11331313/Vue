d3ee5775bf09848b6d7687046dda9f1615625535
skygodbas <2238178090@qq.com>
2017-11-22 18:46:08 +0800

    计数

---
+++ src/components/Count.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+      <h1>计数器：{{$store.state.count}}</h1>
+      <button @click="$store.commit('add')">增加</button>
+      <button @click="$store.commit('reduce')">减少</button>
+  </div>
+</template>
+
+<script>
+import store from '@/vuex/store'
+export default {
+  data () {
+      return {}
+  },
+  store
+}
+</script>
+
+<style scoped>
+
+</style>
